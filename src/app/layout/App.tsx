7144d1f7ce34525e199b0c48bba46677d1bd557d
pulkitsrivastava9 <pulkitsrivastava9@gmail.com>
2021-06-02 16:39:01 +0400

    added agent.ts for axios

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,6 +6,7 @@ import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -16,11 +17,9 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios
-      .get<Activity[]>("http://localhost:5000/api/Activities")
-      .then((response) => {
-        setActivities(response.data);
-      });
+    agent.Activities.list().then((response) => {
+      setActivities(response);
+    });
   }, []);
 
   function handleSelectActivity(id: string) {
